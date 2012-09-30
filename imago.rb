5406464cbf78aec8c4c227ca6bb9cd210e2fe6b1
Travis Berry <contact@travisberry.com>
2012-09-30 13:51:38 -0600

    added image support

--- imago.rb
+++ imago.rb
@@ -73,7 +73,8 @@ get '/get_image?' do
     content_type :json
     { :link => @link, :website => "http://#{params['website']}" }.to_json
   elsif params['format'] == "image"
-    send_file @link, :type => 'image/jpeg',
+    data = open(@link).read
+    send_file data, :type => 'image/jpeg',
       :disposition => 'inline'
   end
 end
