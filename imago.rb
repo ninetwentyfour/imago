8dc44152664ac23db65fae6f792d8aee8f92477f
Travis Berry <contact@travisberry.com>
2012-09-29 18:47:43 -0600

    added s3

--- imago.rb
+++ imago.rb
@@ -1,5 +1,9 @@
-%w(rubygems sinatra sinatra-initializers imgkit).each{ |g| require g }
+%w(rubygems sinatra sinatra-initializers imgkit aws/s3).each{ |g| require g }
+# require 'aws/s3'
 
+set :bucket, 'screengrab-test'
+set :s3_key, ENV['S3_KEY']
+set :s3_secret, ENV['S3_SECRET']
 # register Sinatra::Initializers
 
 get '/?' do
@@ -9,7 +13,11 @@ get '/?' do
   
   html = "http://google.com"
   kit   = IMGKit.new('http://google.com')
-  kit.to_file('./.tmp/test.png')
+  #kit.to_file('./.tmp/test.png')
+  AWS::S3::Base.establish_connection!(
+  :access_key_id     => settings.s3_key,
+  :secret_access_key => settings.s3_secret)
+  AWS::S3::S3Object.store('test.png',open(kit.to_img(:png)),settings.bucket,:access => :public_read)
 
   #puts "#{settings.root}/bin/wkhtmltoimage-amd64"
 end
