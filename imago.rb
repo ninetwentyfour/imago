6bdf2a89a08215a439af7af844ec4e67004ce9c4
Travis Berry <contact@travisberry.com>
2012-09-29 19:06:25 -0600

    trying to take website as param

--- imago.rb
+++ imago.rb
@@ -1,5 +1,4 @@
-%w(rubygems sinatra sinatra-initializers imgkit aws/s3).each{ |g| require g }
-# require 'aws/s3'
+%w(rubygems sinatra sinatra-initializers imgkit aws/s3 digest/md5).each{ |g| require g }
 
 set :bucket, 'screengrab-test'
 set :s3_key, ENV['S3_KEY']
@@ -7,17 +6,31 @@ set :s3_secret, ENV['S3_SECRET']
 # register Sinatra::Initializers
 
 get '/?' do
+  # take website and sizes as params (maybe jpg or png)
+  
+  # hash params to use as file name and key value
+  
+  # lookup a key first, if found return that
+  
+  # if not - go ahead and get the image 
+  
+  # save memcache or redis key for has with final image url
+  
+  # check for valid url
+  
+  # check that image exists or return some default not found with an error message 
+  
+  # move this into real config file
   IMGKit.configure do |config|
     config.wkhtmltoimage = "#{settings.root}/bin/wkhtmltoimage-amd64"
   end
   
-  html = "http://google.com"
-  kit   = IMGKit.new('http://google.com')
-  #kit.to_file('./.tmp/test.png')
+  html = "http://#{params['website']}"
+  name = Digest::MD5.hexdigest(html)
+  kit   = IMGKit.new(html)
+
   AWS::S3::Base.establish_connection!(
   :access_key_id     => settings.s3_key,
   :secret_access_key => settings.s3_secret)
-  AWS::S3::S3Object.store('test.png',kit.to_img(:png),settings.bucket,:access => :public_read)
-
-  #puts "#{settings.root}/bin/wkhtmltoimage-amd64"
+  AWS::S3::S3Object.store(name,kit.to_img(:png),settings.bucket,:access => :public_read)
 end
