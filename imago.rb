c3b440a6219661ca0e2839e31a4a93dbd954fd1a
Travis Berry <contact@travisberry.com>
2012-09-30 19:50:45 -0600

    added expires

--- imago.rb
+++ imago.rb
@@ -97,6 +97,7 @@ def respond(link, params)
     # set headers accordingly (all that apply)
     headers 'Content-Type' => head['Content-Type']
     headers 'Cache-Control' => "max-age=2592000, no-transform, public"
+    headers 'Expires' => "Thu, 29 Sep 2022 01:22:54 GMT+00:00"
 
     # stream back the contents
     stream do |out|
