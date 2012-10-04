057dcc7e64d336a028b6ca9e578d255feb26d867
Travis Berry <contact@travisberry.com>
2012-10-03 18:25:25 -0600

    more tests

--- spec/imago_spec.rb
+++ spec/imago_spec.rb
@@ -32,7 +32,7 @@ describe 'Imago' do
     it "returns a json response for a valid url" do
       get '/get_image?website=www.travisberry.com&width=320&height=200&format=json'
       last_response.should be_ok
-      last_request.env["Content-Type"].should == "application/json"
+      last_response.header["Content-Type"].should == "application/json"
       last_response.body.should == '{"link":"http://d29sc4udwyhodq.cloudfront.net/6b3927a0e37512e2efa3b25cb440a498.jpg","website":"http://www.travisberry.com"}'
     end
     
@@ -45,7 +45,7 @@ describe 'Imago' do
     it "returns a html response for a valid url" do
       get '/get_image?website=www.travisberry.com&width=320&height=200&format=html'
       last_response.should be_ok
-      last_request.env["Content-Type"].should == "text/html"
+      last_response.header["Content-Type"].should == "text/html"
       assigns(:link).should_not be_blank
     end
 end
