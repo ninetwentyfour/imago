497900dc9aaa756d071ce0c0c3b68e8954f8aecf
Travis Berry <contact@travisberry.com>
2012-10-28 14:47:02 -0600

    added async

--- imago.rb
+++ imago.rb
@@ -10,7 +10,30 @@ include Magick
 
 class Imago < Sinatra::Base
   register Sinatra::Async
-  require_relative 'config'
+  # s3 configs
+  set :bucket, ENV['S3_BUCKET']
+  set :s3_key, ENV['S3_KEY']
+  set :s3_secret, ENV['S3_SECRET']
+  
+  # redis configs
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  
+  # imgkit configs
+  IMGKit.configure do |config|
+    config.wkhtmltoimage = "#{settings.root}/bin/wkhtmltoimage-amd64"
+  end
+  
+  if ENV['RACK_ENV'] == 'production'
+    # airbrake configs
+    Airbrake.configure do |config|
+      config.api_key = ENV['AIRBRAKE_API_KEY']
+    end
+  
+    # use airbrake errors
+    use Airbrake::Rack
+    enable :raise_errors
+  end
   #### GET /get_image?
 
   # `/get_image?` takes a list of params.
