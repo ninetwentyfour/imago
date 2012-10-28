defb861a588de5162ed2824d591c5c7aa39d6385
Travis Berry <contact@travisberry.com>
2012-10-28 15:09:22 -0600

    trying fibers

--- imago.rb
+++ imago.rb
@@ -54,7 +54,8 @@ get '/get_image?' do
         thumb.write temp_file
 
         # Store the image on s3.
-        send_to_s3(temp_file, name)
+        # send_to_s3(temp_file, name)
+        http_get(file, name)
 
         # Create the link.
         @link = "http://static-stage.imago.in.s3.amazonaws.com/#{name}.jpg"
@@ -193,4 +194,37 @@ def send_to_s3(file, name)
       puts "Upload finished!"; EM.stop 
     end
   end
+end
+
+def http_get(file, name)
+  # get the fiber our browser request is being processed in
+  calling_fiber = Fiber.current
+
+  # make an async request
+  # EventMachine queues the http.callback block and executes it at some point after the http request returns
+  # http = EventMachine::HttpRequest.new( url ).get
+  # http.callback { calling_fiber.resume( http ) }
+  
+  # file = "#{settings.root}/bin/big_image.jpeg"
+  headers = {'Cache-Control' => "max-age=252460800", 
+             'Content-Type' => 'image/jpeg', 
+             'Expires' => 'Fri, 16 Nov 2018 22:09:29 GMT'}
+  on_error = Proc.new {|http| logger.info "amazon error"; EM.stop }
+  on_success = Proc.new {|http| calling_fiber.resume(http) }
+  item = Happening::S3::Item.new( settings.bucket, "#{name}.jpg",
+                                  :aws_access_key_id => settings.s3_key, 
+                                  :aws_secret_access_key => settings.s3_secret,
+                                  :permissions => 'public-read'
+                                )
+  item.put( File.read(file), :on_error => on_error, :on_success => on_success, :headers => headers ) do |response|
+    logger.info "trying uload"
+    puts "Upload finished!"
+    calling_fiber.resume(item)
+  end
+  
+
+  # the calling_fiber yields control back to the EM reactor, which can continue processing other browser requests etc
+  # When the async http request returns, the callback block is queued for EM to execute when it received control again.
+  # The callback resumes the calling fiber, and the get method returns the result of the http get request.
+  return calling_fiber.yield
 end
